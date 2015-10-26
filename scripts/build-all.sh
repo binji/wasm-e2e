3d67f1f565a5d7b728b13df0a44d46f7320b8e16
Ben Smith <binji@chromium.org>
2015-10-26 14:23:05 -0700

    Add script to build LLVM, d8 and sexpr-wasm

---
+++ scripts/build-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+${SCRIPT_DIR}/build-llvm.sh
+${SCRIPT_DIR}/build-d8.sh
+${SCRIPT_DIR}/build-sexpr-wasm.sh
