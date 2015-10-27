79b6b802b069e3360b81783bb92033af6217ec5a
Ben Smith <binji@chromium.org>
2015-10-27 13:18:28 -0700

    Extend clone depth of LLVM to 5000

--- scripts/update-llvm.sh
+++ scripts/update-llvm.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
 LLVM_DIR="${ROOT_DIR}/third_party/llvm"
 CLANG_DIR="${LLVM_DIR}/tools/clang"
+DEPTH=5000
 
 LLVM_SHA=644d8eaae43035481913aa898626305c59343f85
 CLANG_SHA=e90410ed3ac353b1bbc98970b2954ffeff65ab16
@@ -15,9 +16,14 @@ fetch_or_clone() {
   local url=$2
 
   if [[ -d ${dir} ]]; then
-    git -C ${dir} fetch origin
+    # Try to extend the depth if needed.
+    if [[ `git -C ${dir} log --pretty=oneline | wc -l` -lt ${DEPTH} ]]; then
+      git -C ${dir} fetch origin --depth ${DEPTH}
+    else
+      git -C ${dir} fetch origin
+    fi
   else
-    git -C "$(dirname ${dir})" clone --depth 1 $url
+    git -C "$(dirname ${dir})" clone --depth ${DEPTH} $url
   fi
 }
 
