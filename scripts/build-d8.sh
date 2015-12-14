b208c5e5d0aacf1997ef04d51ac9e9323bf1d7f4
Nick Bray <ncbray@chromium.org>
2015-12-14 15:32:23 -0800

    Use the upstream V8 repo rather than v8-native-prototype.
    Fixes #12

--- scripts/build-d8.sh
+++ scripts/build-d8.sh
@@ -24,7 +24,7 @@ done
 SCRIPT_DIR="$(dirname "$0")"
 ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
 
-cd ${ROOT_DIR}/third_party/v8-native-prototype
+cd ${ROOT_DIR}/third_party
 
 # copied from v8-native-prototype/install-dependencies.sh, but this fetches a
 # shallow clone.
@@ -35,29 +35,20 @@ fi
 
 export PATH=$PWD/depot_tools:$PATH
 
-if [[ ${sync} = "YES" ]]; then
-  if [[ ! -d v8 ]]; then
-    echo "Fetching v8"
-    mkdir v8
-    pushd v8
-    fetch --no-history v8
-    ln -fs $PWD/.. v8/third_party/wasm
-    popd
-  else
-    pushd v8/v8
-    git fetch origin
-    git checkout origin/master
-    popd
-  fi
+cd v8
 
-  pushd v8
+# Create a .gclient file if it doesn't exist.
+if [[ ! -e .gclient ]]; then
+  gclient config --unmanaged `cd v8; git config --get remote.origin.url`
+fi
+
+if [[ ${sync} = "YES" ]]; then
   gclient update
-  popd
 fi
 
 # Don't use the CC from the environment; v8 doesn't seem to build properly with
 # it.
 unset CC
-cd v8/v8
+cd v8
 GYP_GENERATORS=ninja build/gyp_v8 -Dv8_wasm=1 -Dwerror=
 time ninja -C out/${config} d8
