8066e32e440dcc4ece43a662196d484d1b0ba1bc
Michal Cierniak <cierniak@users.noreply.github.com>
2016-01-14 17:05:23 -0800

    Made run.py accept .c and .wast inputs

--- scripts/run.py
+++ scripts/run.py
@@ -34,7 +34,7 @@ def call_and_suppress_stderr(cmd):
 
 def main(args):
   parser = argparse.ArgumentParser()
-  parser.add_argument('c_file', help='test C file')
+  parser.add_argument('in_file', help='input file (C or WAST)')
   parser.add_argument('--js-env', help='JS/WASM environment file')
   parser.add_argument('--native-env', help='native environment file')
   parser.add_argument('--dump', help='directory to store intermediate files')
@@ -43,7 +43,9 @@ def main(args):
   if not options.js_env and not options.native_env:
     options.js_env = WASM_JS
 
-  basename = os.path.splitext(os.path.basename(options.c_file))[0]
+  path_parts = os.path.splitext(os.path.basename(options.in_file))
+  basename = path_parts[0]
+  extension = path_parts[1]
   if not options.dump:
     clean_temp_dir = True
     temp_dir = tempfile.mkdtemp(prefix='wasm-e2e-run-')
@@ -55,18 +57,22 @@ def main(args):
 
   try:
     if options.js_env:
-      s_file = os.path.join(temp_dir, basename + '.s')
-      wast_file = os.path.join(temp_dir, basename + '.wast')
+      if extension == '.c':
+        s_file = os.path.join(temp_dir, basename + '.s')
+        wast_file = os.path.join(temp_dir, basename + '.wast')
+        subprocess.check_call([WACC, '-fno-builtin', options.in_file, '-o', s_file, '-I', '.'])
+        subprocess.check_call([LINKER, s_file, '-o', wast_file])
+      elif extension == '.wast':
+        wast_file = options.in_file
+
       wasm_file = os.path.join(temp_dir, basename + '.wasm')
 
-      subprocess.check_call([WACC, '-fno-builtin', options.c_file, '-o', s_file, '-I', '.'])
-      subprocess.check_call([LINKER, s_file, '-o', wast_file])
       subprocess.check_call([SEXPR_WASM, wast_file, '-o', wasm_file])
       call_and_suppress_stderr([D8, '--expose-wasm', options.js_env, '--', wasm_file])
 
     if options.native_env:
       executable_file = os.path.join(temp_dir, basename)
-      subprocess.check_call([CLANG, '-O2', options.c_file, options.native_env,
+      subprocess.check_call([CLANG, '-O2', options.in_file, options.native_env,
                              '-o', executable_file, '-I', '.'])
       subprocess.check_call([executable_file])
 
